31db47783d1ffd38ed0d6c8145c0dee42bcf9b30
Domantas Martinaitis <domantas.martinaitis@gmail.com>
2021-10-06 11:19:19 +0200

    First triangle

--- MinecraftClone/src/main.cpp
+++ MinecraftClone/src/main.cpp
@@ -1,7 +1,45 @@
-#include<iostream>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-	std::cout << "Hello World!";
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Create a triangle with legacy OpenGL */
+        glBegin(GL_TRIANGLES);
+        glVertex3f(-0.7f, -0.7f, -0.7f);
+        glVertex3f(0, 0.7f, 0);
+        glVertex3f(0.7f, -0.7f, 0.7f);
+        glEnd();
+
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
