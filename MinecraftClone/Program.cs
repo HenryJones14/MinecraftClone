7016dff119a99cb886f68e5f8da4e9c94841306d
Domantas Martinaitis <domantas.martinaitis@gmail.com>
2021-11-09 13:07:00 +0100

    Hello again triangle

---
+++ MinecraftClone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinecraftClone;
+
+class Program
+{
+    public static int Main()
+    {
+        using (MainGame game = new MainGame(1280, 720, "MinecraftClone"))
+        {
+            //Run takes a double, which is how many frames per second it should strive to reach.
+            //You can leave that out and it'll just update as fast as the hardware will allow it.
+            game.Run(144);
+        }
+
+
+        return 0;
+    }
+}
--- MinecraftClone/src/MainGame.cs
+++ MinecraftClone/src/MainGame.cs
@@ -1,13 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using System;
 
 namespace MinecraftClone
 {
-    class MainGame
+    public class MainGame : GameWindow
     {
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+        };
 
+        int VertexBufferObject;
+        int VertexArrayObject;
+        Shader MainShader;
+
+        public MainGame(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+            Console.WriteLine("Window created: ({0}, {1})", width, height);
+        }
+
+        #region Rendering
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            MainShader = new Shader("shaders/shader.vert", "shaders/shader.frag");
+            MainShader.Use();
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            MainShader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            Context.SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
+
+            MainShader.Dispose();
+            base.OnUnload(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        #endregion
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            
+            KeyboardState input = Keyboard.GetState();
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+        }
     }
 }
